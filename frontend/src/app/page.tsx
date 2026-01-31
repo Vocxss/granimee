6784c8f7c3edb88238575e6648c7ee3db6f5ae68
Muh Tsaqif Hafidz <142965056+Vocxss@users.noreply.github.com>
2026-01-31 22:23:35 +0700

    Refactor Page component to use redirect

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,5 @@
-"use client"
+import { redirect } from "next/navigation";
 
-import React from "react"
-
-
-export default function Page () {
-    React.useEffect(() => {
-        if (window.location.pathname === "/") window.location.href = "/home"    
-    })
-    return
-} 
+export default function Page() {
+  redirect("/home");
+}
