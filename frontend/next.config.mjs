dfa7afb76f68a23f8a6f0f39cb303907cf9f60ad
Muh Tsaqif Hafidz <142965056+Vocxss@users.noreply.github.com>
2026-01-31 22:41:28 +0700

    Add JSDoc type annotation for NextConfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,5 @@
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async redirects() {
     return [
       {
@@ -9,3 +10,5 @@ export default {
     ];
   },
 };
+
+export default nextConfig;
